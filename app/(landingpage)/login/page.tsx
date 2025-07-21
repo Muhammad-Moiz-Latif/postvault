9b03c56e636aaf936dd09740ad4ee64c4871869b
Muhammad-Moiz-Latif <moizlatifm@gmail.com>
2025-07-22 00:52:26 +0500

    created signup page

--- app/(landingpage)/login/page.tsx
+++ app/(landingpage)/login/page.tsx
@@ -1,7 +1,46 @@
-export default function LoginPage(){
-    return(
+import bgImage from '../../../assets/hand-drawn-business-communication-concept.png';
+import logo from '../../../assets/POSTVAULT.png';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+    weight: ['400', '700', '600', '500'],
+    subsets: ['latin'],
+});
+
+
+export default function LoginPage() {
+    return (
         <>
-            <div>This is the login page</div>
+            <div className="max-w-full h-screen flex justify-center items-center bg-sky-200 p-8">
+                <div className="bg-white rounded-xl w-full h-full flex">
+                    <div className='w-1/2 rounded-xl relative m-3 bg-sky-50'>
+                        <img src={bgImage.src} className='w-full' />
+                        <h1 className={`text-center ${roboto.className} text-2xl`}>Every story deserves a safe place — welcome to</h1>
+                        <img src={logo.src} className='w-52 absolute -bottom-9 right-52 ' />
+                    </div>
+                    <div className={`w-1/2 rounded-r-xl bg-white flex flex-col py-5 px-24 ${roboto.className}`}>
+                        <h1 className='text-[40px] font-medium'>Create an account</h1>
+                        <h1 className='text-sm mt-2'>Already have an account? <span className='text-sky-600 hover:cursor-pointer'>Log in</span></h1>
+                        <form className='flex flex-col gap-3 mt-10'>
+                            <input className='w-full h-11 rounded-md pl-4 focus:outline-sky-600 bg-sky-100 placeholder:text-gray-400' placeholder='Username' />
+                            <input className='w-full h-11 rounded-md pl-4 focus:outline-sky-600 bg-sky-100 placeholder:text-gray-400' placeholder='Email' type='email'/>
+                            <input className='w-full h-11 rounded-md pl-4 focus:outline-sky-600 bg-sky-100 placeholder:text-gray-400' placeholder='Password' type='password'/>
+                            <div className='mt-1 flex items-center gap-3 mb-5'>
+                                <input type='checkbox' className='w-5 h-5' />
+                                <label className='text-sm'>I agree to the <span className='underline text-sky-600 hover:cursor-pointer'>Terms & Conditions</span></label>
+                            </div>
+                            <button className='w-full h-11 rounded-md text-white bg-sky-600 mb-3'>Create account</button>
+                        </form>
+                        <div className='flex justify-evenly items-center mb-3'>
+                            <hr className=" flex-grow border-sky-400" />
+                            <h1 className='text-xs text-sky-400 mx-4'>Or register with</h1>
+                            <hr className=" flex-grow border-sky-400" />
+                        </div> 
+                        <button className='w-full h-11 rounded-md text-white bg-sky-600'>Google button</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
+
--- app/@modal/(.)login/page.tsx
+++ app/@modal/(.)login/page.tsx
@@ -2,10 +2,12 @@
 import { useRouter } from 'next/navigation';
 import { useEffect, useRef } from 'react';
 import Signin from "../../../assets/login.png";
+import Exit from '../../../assets/reject.png';
 import { Roboto } from "next/font/google"
+import Link from 'next/link';
 
 const roboto = Roboto({
-  weight: ['400','700','600'],
+  weight: ['400', '700', '600'],
   subsets: ['latin'],
 });
 
@@ -15,36 +17,45 @@ export default function LoginPage() {
   const router = useRouter();
 
   useEffect(() => {
+    console.log('i am working');
     if (dialogRef.current && !dialogRef.current.open) {
       dialogRef.current.showModal();
     }
   }, []);
 
+
   return (
-    <dialog
-      ref={dialogRef}
-      className="rounded-4xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4 py-6 max-w-sm w-full shadow-lg backdrop:bg-black/50 h-[28rem] bg-gradient-to-b from-sky-200 via-sky-50 to-white "
-    >
-      <form className={`flex flex-col justify-center gap-2 ${roboto.className}`}>
-        <div className='flex justify-center '>
-          <img src={Signin.src} className='w-12 bg-white shadow-md shadow-sky-200 p-3 rounded-2xl' />
-        </div>
-        <h1 className={`text-center text-xl font-semibold ${roboto.className}`}>Sign in with email</h1>
-        <input className='outline-0 bg-gray-200 p-1 rounded-xl pl-2 placeholder:text-sm mx-3' type='email' placeholder='Email' />
-        <input className='outline-0 bg-gray-200 p-1 rounded-xl pl-2 placeholder:text-sm mx-3' type='password' placeholder='Password' />
-        <div className='flex justify-end mr-6 text-sm mb-3'>
-          <h1>Forgot passsword?</h1>
-        </div>
-        <button type='button' className='bg-black text-white py-2 rounded-xl mx-3 text-sm mb-2'>Get Started</button>
-        <div className="flex justify-center items-center gap-2 mb-2">
-          <div className='flex-grow border-dotted border-t-4 border-t-gray-200 ml-3'></div>
-          <h1 className="text-sm text-gray-500 tracking-wide">Or sign in with</h1>
-          <div className='border-t-4 flex-grow border-dotted border-t-gray-200 mr-3'></div>
-        </div>
-        <button className='bg-black text-white p-2 rounded-xl'>
-          Google
+    <div>
+      <dialog
+        ref={dialogRef}
+        className="rounded-4xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 px-4 py-6 max-w-sm w-full shadow-lg backdrop:bg-black/50 h-[28rem] bg-gradient-to-b from-sky-200 via-sky-50 to-white "
+      >
+        <button className='absolute top-3 right-3 hover:cursor-pointer' onClick={()=>router.back()}>
+          <img src={Exit.src} className='w-7'/>
         </button>
-      </form>
-    </dialog>
+        <form className={`flex flex-col justify-center gap-2 ${roboto.className}`}>
+          <div className='flex justify-center '>
+            <img src={Signin.src} className='w-12 bg-white shadow-md shadow-sky-200 p-3 rounded-2xl' />
+          </div>
+          <h1 className={`text-center text-xl font-semibold ${roboto.className}`}>Sign in with email</h1>
+          <input className='outline-0 bg-gray-200 p-1 rounded-xl pl-2 placeholder:text-sm mx-3' type='email' placeholder='Email' />
+          <input className='outline-0 bg-gray-200 p-1 rounded-xl pl-2 placeholder:text-sm mx-3' type='password' placeholder='Password' />
+          <div className='flex justify-end mr-6 text-sm mb-3'>
+            <h1>Forgot passsword?</h1>
+          </div>
+          <button type='button' className='bg-black text-white py-2 rounded-xl mx-3 text-sm mb-2'>Get Started</button>
+          <div className="flex justify-center items-center gap-2 mb-2">
+            <div className='flex-grow border-dotted border-t-4 border-t-gray-200 ml-3'></div>
+            <h1 className="text-sm text-gray-500 tracking-wide">Or sign in with</h1>
+            <div className='border-t-4 flex-grow border-dotted border-t-gray-200 mr-3'></div>
+          </div>
+          <button className='bg-black text-white p-2 rounded-xl'>
+            Google
+          </button>
+          <p className='text-center mt-2'>Don't have an account? <button className='text-blue-400 hover:cursor-pointer' onClick={() => router.replace('/login')}> Sign Up</button></p>
+        </form>
+      </dialog>
+    </div>
+
   );
 }
